890e02c675844bbcbb16fa48caea06ad90c25116
Vedat DOKUZKARDES <124203274+vedatdokuzkardes@users.noreply.github.com>
2023-12-25 20:14:15 +0300

    Add files via upload

---
+++ AppStoreTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AppStoreTest
+//
+//  Created by Vedat Dokuzkardeş on 24.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+
+}
+
